30294378cf70b6041c397e559754b0a94d9c6944
Adam Jaworski <akjaworski@gmail.com>
2025-08-03 00:56:09 +0100

    ready for testing

--- easyio_configuration_updater.ps1
+++ easyio_configuration_updater.ps1
@@ -15,106 +15,22 @@ param(
     [string]$ParameterFile
 )
 
-if (-not $ParameterFile) {
-    Write-Host "Usage: $($MyInvocation.MyCommand.Name) [parameter_file]"
-    exit 1
-}
 
-foreach ($line in Get-Content "$ParameterFile") {
-    if ($line -match '=') {
-        Invoke-Expression "$line"
-    }
-}
-
-# Check that all required parameters are provided
-foreach ($parameter_name in "BackupProjectDirectory", "OutputProjectDirectory", "KeysDirectory", `
-             "project_id", "registry_id", "region_id", "mqtt_host_id", "ca_file_name") {
-    $parameter_value = $(Get-Variable -Name $parameter_name -ValueOnly)
-    if (-not $parameter_value -or $parameter_value -eq "") {
-        Write-Host "Parameter is not defined: $parameter_name"
-        if (-not $parameter_name -eq "KeysDirectory") {
-            Write-Host "Quitting."
-            exit 1
-        }
-        else {
-            Write-Host "Note: keys directory not provided, existing keys in backups will be retained."
-        }
-    }
-}
-
-# Check that the provided directories exist on valid paths
-foreach ($dir in $BackupProjectDirectory, $OutputProjectDirectory, $KeysDirectory) {
-    if ($dir -and -not (Test-Path -Path $dir -PathType Container)) {
-        Write-Host "$dir does not exist, quitting." | Write-Error
-        exit 1
-    }
-}
-
-Write-Host "Using parameters:"
-Write-Host "backup project directory:     $BackupProjectDirectory"
-Write-Host "output project directory:     $OutputProjectDirectory"
-Write-Host "keys directory:               $KeysDirectory"
-Write-Host "project_id:                   $project_id"
-Write-Host "registry_id:                  $registry_id"
-Write-Host "region_id:                    $region_id"
-Write-Host "mqtt_host:                    $mqtt_host_id"
-Write-Host "ca_file:                      $ca_file_name"
-
-exit 1
-
-# Function to identify the device name
-function Identify-DeviceName {
+# Function to identify the device names
+function Identify-DeviceNames {
     param (
         [string]$ExpandedArchivePath
     )
     $ParameterFile = "$ExpandedArchivePath\cpt\plugins\DataServiceConfig\data_mapping.json"
-
-    $DeviceName = Select-String -Path $ParameterFile -Pattern '"device_id":"([^"]+)"' | % {$_.matches.groups[1].value} | Select-Object -First 1
-    return $DeviceName
+    $AllDeviceNames = Select-String -Path $ParameterFile -Pattern '"device_id":"([^"]+)"' -AllMatches | `
+        % {$_.matches.groups | Where-Object {$_.Name -eq 1}} | % {$_.Value} 
+    return $AllDeviceNames
 }
-
-
-
 
 
 function Update-CloudSettings {
     param (
-        [string]$rootDir,
-        [string]$project_id,
-        [string]$registry_id,
-        [string]$region_id,
-        [string]$mqtt_host_id,
-        [string]$ca_file_name
-    )
-
-    $configurationPath = Join-Path $rootDir 'cpt/plugins/DataServiceConfig'
-    $jsonFile = Join-Path $configurationPath 'data_mapping.json'
-    $backupFile = Join-Path $configurationPath 'data_mapping.old.json'
-
-    # Read JSON file contents
-    $content = Get-Content $jsonFile -Raw
-
-    # Apply substitutions
-    $content = $content -replace '"project_id":"[^"]+"', "`"project_id`":`"$project_id`""
-    $content = $content -replace '"registry_id":"[^"]+"', "`"registry_id`":`"$registry_id`""
-    $content = $content -replace '"region":"[^"]+"', "`"region`":`"$region_id`""
-    $content = $content -replace '"mqtt_host":"[^"]+"', "`"mqtt_host`":`"$mqtt_host_id`""
-    $content = $content -replace '"key_file":"[^A-Z0-9]+([A-Z0-9\-]+)\.pem"', '"key_file":"rsa_private_$1.pem"'
-    $content = $content -replace '"cert_file":"[^A-Z0-9]+([A-Z0-9\-]+)\.pem"', '"cert_file":"rsa_cert_$1.pem"'
-    $content = $content -replace '"ca_file":"[^"]+"', "`"ca_file`":`"$ca_file_name`""
-
-    # Backup original file and write new content
-    Move-Item -Path $jsonFile -Destination $backupFile -Force
-    $content | Set-Content -Path $jsonFile
-    Remove-Item -Path $backupFile -Force
-}
-
-
-
-function Update-CloudSettings {
-    param (
-        [string]$ExpandedBackupRoot,
-        [string]$DeviceName
+        [string]$ExpandedBackupRoot
     )
     # This function also uses global variables project_id, registry_id, region_id, mqtt_host_id
     # ca_file_name
@@ -127,58 +43,49 @@ function Update-CloudSettings {
     $content = Get-Content $OldFile -Raw
 
     # Apply substitutions
-    $content = $content -replace '"project_id":"[^"]+"', "`"project_id`":`"$project_id`""
-    $content = $content -replace '"registry_id":"[^"]+"', "`"registry_id`":`"$registry_id`""
-    $content = $content -replace '"region":"[^"]+"', "`"region`":`"$region_id`""
-    $content = $content -replace '"mqtt_host":"[^"]+"', "`"mqtt_host`":`"$mqtt_host_id`""
-    $content = $content -replace '"key_file":"[^A-Z0-9]+([A-Z0-9\-]+)\.pem"', '"key_file":"rsa_private_$1.pem"'
-    $content = $content -replace '"cert_file":"[^A-Z0-9]+([A-Z0-9\-]+)\.pem"', '"cert_file":"rsa_cert_$1.pem"'
-    $content = $content -replace '"ca_file":"[^"]+"', "`"ca_file`":`"$ca_file_name`""
-    $content = $content -replace "`r`n", "`n"
+    $content = $content -creplace '"project_id":"[^"]+"', "`"project_id`":`"$project_id`""
+    $content = $content -creplace '"registry_id":"[^"]+"', "`"registry_id`":`"$registry_id`""
+    $content = $content -creplace '"region":"[^"]+"', "`"region`":`"$region_id`""
+    $content = $content -creplace '"mqtt_host":"[^"]+"', "`"mqtt_host`":`"$mqtt_host_id`""
+    $content = $content -creplace '"key_file":"[^A-Z0-9]+([A-Z]+)-?([0-9]+)\.pem"', '"key_file":"rsa_private_$1-$2.pem"'
+    $content = $content -creplace '"cert_file":"[^A-Z0-9]+([A-Z]+)-?([0-9]+)\.pem"', '"cert_file":"rsa_cert_$1-$2.pem"'
+    $content = $content -creplace '"ca_file":"[^"]+"', "`"ca_file`":`"$ca_file_name`""
+    $content = $content -creplace "`r`n", "`n"
 
     # The -Encoding utf8 causes Powershell to use Unix-type line endings and will not have a BOM
     # from Powershell version 6 onwards. For compatibility with older Powershell (shipped with
     # Windows 10), we use -Encoding ASCII to prevent the BOM from being inserted
     $content | Out-File -FilePath $NewFile -Encoding ascii -NoNewLine
 
-    # In Powershell, we can't rename a file onto a destination that already exists, so use 'Move-Item' instead    
+    # In Powershell, we can't rename a file onto a destination that already exists, so we use 'Move-Item'
+    # instead    
     Move-Item -Path $NewFile -Destination $OldFile -Force
 }
 
 function Update-Keys {
     param (
         [string]$ExpandedBackupRoot,
-        [string]$DeviceName
+	[string]$KeysDirectory,
+	[string]$CAFile,
+        [string]$AllDeviceNames
     )
 
     $KeysPath = "$ExpandedBackupRoot\cpt\plugins\DataServiceConfig\uploads\certs"
-    $PrivateKey = "rsa_private$DeviceName.pem"
-    $PublicKey = "rsa_public$DeviceName.pem"
-    $CAFile = "CA File.pem"
 
     # Clear old keys
     Remove-Item "$KeysPath\*" -Force -ErrorAction SilentlyContinue
 
-    # Copy new ones
-    Copy-Item "$KeysDirectory\$PrivateKey" "$KeysPath\$PrivateKey"
-    Copy-Item "$KeysDirectory\$PublicKey" "$KeysPath\$PublicKey"
+    # Copy new CA file
     Copy-Item "$KeysDirectory\$CAFile" "$KeysPath\$CAFile"
 
-    # Update key references in configuration file
-    $ConfigurationPath = "$ExpandedBackupRoot\cpt\plugins\DataServiceConfig"
-    $OldFile = "$ConfigurationPath\data_mapping.json"
-    $NewFile = "$ConfigurationPath\data_mapping.updated.json"
+    ForEach ($DeviceName in $AllDeviceNames.split()) {
+        $PrivateKeyFile = "rsa_private_$DeviceName.pem"
+        $CertFile = "rsa_cert_$DeviceName.pem"
+        # Copy new ones
+        Copy-Item "$KeysDirectory\$DeviceName\rsa_private.pem" "$KeysPath\$PrivateKeyFile"
+        Copy-Item "$KeysDirectory\$DeviceName\rsa_cert.pem" "$KeysPath\$CertFile"
+    }
 
-    # The -Encoding utf8 causes Powershell to use Unix-type line endings and will not have a BOM
-    # from Powershell version 6 onwards. For older Powershell (shipped with Windows 10), use -Encoding ASCII
-    # to prevent the BOM from being inserted
-    (Get-Content $OldFile) -replace '"rsa_private[A-Z0-9]*\.pem"', "`"rsa_private$DeviceName.pem`"" `
-        -replace '"rsa_public[A-Z0-9]*\.pem"', "`"rsa_public$DeviceName.pem`"" `
-        -replace "`r`n", "`n" `
-        | Out-File -FilePath $NewFile -Encoding ascii -NoNewLine
-
-    # In Powershell, we can't rename a file onto a destination that already exists, so use 'Move-Item' instead    
-    Move-Item -Path $NewFile -Destination $OldFile -Force
 }
 
 function Update-TimeSettings {
@@ -227,6 +134,70 @@ DST End Time:0,0
 }
 
 
+function Get-Parameters {
+     param (
+        [string]$ParameterFile
+    )
+
+    if (-not $ParameterFile) {
+        Write-Host "Usage: $($MyInvocation.MyCommand.Name) [parameter_file]"
+        exit 1
+    }
+
+    # execute assignment lines only
+    foreach ($line in Get-Content "$ParameterFile") {
+        if ($line -match '=') {
+            Invoke-Expression "$line"
+        }
+    }
+
+    # Check that all required parameters are provided
+    foreach ($parameter_name in "BackupProjectDirectory", "OutputProjectDirectory", "KeysDirectory", `
+                 "project_id", "registry_id", "region_id", "mqtt_host_id", "ca_file_name") {
+	$parameter_value = $(Get-Variable -Name $parameter_name -ValueOnly)
+	# push the setting into global namespace
+        Set-Variable -Name global:$parameter_name -Value $parameter_value
+        if (-not $parameter_value -or $parameter_value -eq "") {
+            Write-Host "Parameter is not defined: $parameter_name"
+            if (-not $parameter_name -eq "KeysDirectory") {
+                Write-Host "Quitting."
+                exit 1
+            }
+            else {
+                Write-Host "Note: keys directory not provided, existing keys in backups will be retained."
+		$global:KeysDirectory = "" 
+            }
+        }
+    }
+
+    # Check that the provided directories exist on valid paths
+    foreach ($dir in $global:BackupProjectDirectory, $global:OutputProjectDirectory, $global:KeysDirectory) {
+        if ($dir -and -not (Test-Path -Path $dir -PathType Container)) {
+            Write-Host "$dir does not exist, quitting." | Write-Error
+            exit 1
+        }
+    }
+
+    Write-Host "Using parameters:"
+    Write-Host "backup project directory:     $global:BackupProjectDirectory"
+    Write-Host "output project directory:     $global:OutputProjectDirectory"
+    Write-Host "keys directory:               $global:KeysDirectory"
+    Write-Host "project_id:                   $global:project_id"
+    Write-Host "registry_id:                  $global:registry_id"
+    Write-Host "region_id:                    $global:region_id"
+    Write-Host "mqtt_host:                    $global:mqtt_host_id"
+    Write-Host "ca_file:                      $global:ca_file_name"
+    
+}
+
+
+#
+# Script processing starts here
+# 
+
+# Read configuration parameters from file
+Get-Parameters $ParameterFile
+
 Write-Host "Processing EasyIO backup files in $BackupProjectDirectory..."
 
 foreach ($DeviceDirectory in Get-ChildItem -Path $BackupProjectDirectory -Directory) {
@@ -235,7 +206,8 @@ foreach ($DeviceDirectory in Get-ChildItem -Path $BackupProjectDirectory -Direct
         # Save names of input and output directories, and backup file name, as strings
         $InputDeviceDirectory = "$BackupProjectDirectory\$($DeviceDirectory.Name)"
         $OutputDeviceDirectory = "$OutputProjectDirectory\$($DeviceDirectory.Name)"
-        $LatestBackup = (Get-ChildItem "$BackupProjectDirectory\$($DeviceDirectory.Name)\*.tgz" | Sort-Object Name | Select-Object -Last 1).Name
+        $LatestBackup = (Get-ChildItem "$BackupProjectDirectory\$($DeviceDirectory.Name)\*.tgz" `
+	    | Sort-Object Name | Select-Object -Last 1).Name
         
         # If we've got a valid archive file, then process it
         if (Test-Path -Path "$InputDeviceDirectory\$LatestBackup" -PathType Leaf) {
@@ -249,18 +221,21 @@ foreach ($DeviceDirectory in Get-ChildItem -Path $BackupProjectDirectory -Direct
             tar -xz -C "$OutputDeviceDirectory" -f "$InputDeviceDirectory\$LatestBackup"
 
             # Get the root directory
-            $ExpandedRootDir = (Get-ChildItem -Path $OutputDeviceDirectory -Directory | Select-Object -First 1).Name
+            $ExpandedRootDir = (Get-ChildItem -Path $OutputDeviceDirectory -Directory | `
+	        Select-Object -First 1).Name
 
             # Make the changes and then re-archive the directory with a new name
             if ($ExpandedRootDir) {
-                $DeviceName = Identify-DeviceName "$OutputDeviceDirectory\$ExpandedRootDir"
-                Update-CloudSettings "$OutputDeviceDirectory\$ExpandedRootDir" $DeviceName
+                $AllDeviceNames = Identify-DeviceNames "$OutputDeviceDirectory\$ExpandedRootDir"
+                Update-CloudSettings "$OutputDeviceDirectory\$ExpandedRootDir"
+
                 if ($KeysDirectory) {
-                    Update-Keys "$OutputDeviceDirectory\$ExpandedRootDir" $DeviceName
+                    Update-Keys "$OutputDeviceDirectory\$ExpandedRootDir" $KeysDirectory $ca_file_name $AllDeviceNames
                 }
                 Update-TimeSettings "$OutputDeviceDirectory\$ExpandedRootDir"
 
-                Rename-Item -Path "$OutputDeviceDirectory\$ExpandedRootDir" -NewName "updated_backup"
+                Move-Item -Path "$OutputDeviceDirectory\$ExpandedRootDir" `
+		    -Destination "$OutputDeviceDirectory\updated_backup" -Force
                 tar -czf "$OutputDeviceDirectory\updated_backup.tgz" -C "$OutputDeviceDirectory" "updated_backup" 
                 Remove-Item "$OutputDeviceDirectory\updated_backup" -Recurse -Force
             }
